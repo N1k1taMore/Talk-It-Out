d9c3afba74aa57216e5cf8684ac80ae40a363309
Nikita More <136251664+N1k1taMore@users.noreply.github.com>
2024-07-28 19:56:11 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -29,7 +29,17 @@ const connectDB=async()=>{
 dotenv.config()
 app.use(express.json())
 app.use("/images",express.static(path.join(__dirname,"/images")))
-app.use(cors({origin:"http://localhost:5173",credentials:true}))
+const allowedOrigins = ['http://localhost:5173', 'https://vocal-shortbread-218f81.netlify.app'];
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+}));
 app.use(cookieParser())
 app.use("/api/auth",authRoute)
 app.use("/api/users",userRoute)
